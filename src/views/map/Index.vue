96db6dd6b7fb03100b6dd8c6a806899f74bfd11c
litiancheng <2227050573@qq.com>
2022-07-31 22:46:56 +0800

    feat: 添加地图插件

---
+++ src/views/map/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <section id="map-container"></section>
+</template>
+
+<script setup>
+import AMapLoader from '@amap/amap-jsapi-loader';
+const map = shallowRef(null)
+AMapLoader.load({
+    "key": "4f27dd8287029a022ac939e19a5afb81",              
+    "plugins": []          
+}).then((AMap)=>{
+    map.value = new AMap.Map('map-container', {
+        zoom: 10,
+        viewMode: '3D',
+        mapStyle: 'amap://styles/97517da16fd09e24f146f72b8e87dfb4'
+    });
+}).catch(e => {
+    console.log('map error', e);
+})
+</script>
+
+<style lang="less" scoped>
+#map-container {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
